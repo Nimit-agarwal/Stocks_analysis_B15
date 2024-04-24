4f398a73d09108b59c866758b89a186efe7ad04c
Nimit Agarwal <E22CSEU0423@bennett.edu.in>
2024-04-24 10:11:13 +0530

    ok

--- streamlit_app/00_🏠_Home.py
+++ streamlit_app/00_🏠_Home.py
@@ -19,7 +19,7 @@ st.markdown("""
     }
     </style>
     """, unsafe_allow_html=True)
-st.markdown("# **🏠 Home**")
+st.markdown("# **🏠 Stock Trading Prediction**")
 st.markdown("""
 We are a team of passionate individuals who believe in the power of data. 
 Our mission is to provide easy and accessible tools for everyone to understand, analyze, and predict the stock market. 
@@ -27,16 +27,18 @@ With our application, we aim to enhance your stock market insights by providing 
 """)
 st.markdown("## **Our Team**")
 st.markdown("""
-- **Nimit Agarwal (E22CSEU0423)**
+- Nimit Agarwal (E22CSEU0423)\n
+- Shivansh Panday (E22CSEU1688)\n
+- Prabhat Thakur (E22CSEU0448)
 """)
 st.markdown("## **Contributions**")
 st.markdown("""
 This application is a result of the collective efforts of our team. 
-Nimit Agarwal, our lead developer has played a pivotal role in bringing this project to life. 
+Nimit Agarwal , our lead developer has played a pivotal role in bringing this project to life. 
 His contributions have been instrumental in the development and success of this application, particularly in the areas of data analysis and stock prediction.
 """)
 st.markdown("## **Contact Us**")
 st.markdown("""
-Feel free to reach out to us at nagarwal2526@gmail.com.
+Email :- nagarwal2526@gmail.com
 """)
 
